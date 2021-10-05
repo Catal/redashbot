f48b48899287b06d59819fd0f6e74565e0998a88
Ryo Nagaoka <ryo.nagaoka@gmail.com>
2021-10-06 06:06:57 +0900

    remove receiver

--- bot.ts
+++ bot.ts
@@ -2,15 +2,16 @@ import { App, ExpressReceiver } from "@slack/bolt";
 import { createApp } from "./src/app";
 import { config } from "./src/config";
 
-const receiver = new ExpressReceiver({
-  signingSecret: config.signingSecret,
-});
+// const receiver = new ExpressReceiver({
+//   signingSecret: config.signingSecret,
+// });
 
-const app = createApp({ ...config, receiver });
+// const app = createApp({ ...config, receiver });
 
-receiver.router.get("/", (_, res) => {
-  res.send("ok");
-});
+// receiver.router.get("/", (_, res) => {
+//   res.send("ok");
+// });
+const app = createApp(config);
 (async () => {
   // Start your app
   await app.start(config.port);
